f02b7939f8935afdbbfad2226b454cc85e431b2b
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2020-10-25 18:53:10 -0700

    Heights example - prediction interval for future height values

--- src/2020-10-26_rethinking-package-demo.R
+++ src/2020-10-26_rethinking-package-demo.R
@@ -9,14 +9,14 @@
 library(tidyverse)
 library(rethinking)
 
-
-# set up the data ---------
+# Example 1: height data --------
+# > set up the data ---------
 data("Howell1")
-d <- Howell1
-d2 <- d[d$age >= 18, ]
+df1 <- Howell1
+df2 <- df1 %>% filter(age >= 18)
 
 
-# define a model ----------
+# > define a model ----------
 flist <- 
     alist(
         height ~ dnorm(mu, sig), 
@@ -25,19 +25,24 @@ flist <-
     )
 
 
-# use quadratic approx to get the posterior ------
-m1 <- quap(flist, data=d2)
+# > use quadratic approx to get the posterior ------
+m1 <- quap(flist, data=df2)
 
 
-# summaries of the posterior ------ 
+# > summaries of the posterior ------ 
 summary(m1)
 precis(m1)
 
 
-# Samples from the posterior ------
+# > samples from the posterior ------
 post_samples <- extract.samples(m1, 100) 
 
 
-# data for prediction interval ----- 
-rnorm(1000, mean = post_samples$mu, sd = post_samples$sig)
+# > data for prediction interval ----- 
+sim_heights <- rnorm(1000, mean = post_samples$mu, sd = post_samples$sig)
+hist(sim_heights)
 
+pred_interval <- quantile(sim_heights, c(.05, .95))
+
+# This gives a prediction interval that should contain the stated proportion of 
+# future height values. 
